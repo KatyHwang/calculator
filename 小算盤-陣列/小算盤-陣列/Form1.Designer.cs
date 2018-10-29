3895547a0aa17d56c03805fc8782d51f5617367d
KatyHwang <37044045+KatyHwang@users.noreply.github.com>
2018-10-29 20:46:07 +0800

    計算機

---
+++ 小算盤-陣列/小算盤-陣列/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace 小算盤_陣列
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CE = new System.Windows.Forms.Button();
+            this.lablshow = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btntotal = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.dot = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnnegative = new System.Windows.Forms.Button();
+            this.btnback = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CE
+            // 
+            this.CE.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.CE.Location = new System.Drawing.Point(185, 146);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(50, 33);
+            this.CE.TabIndex = 101;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
+            this.CE.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // lablshow
+            // 
+            this.lablshow.AutoSize = true;
+            this.lablshow.BackColor = System.Drawing.SystemColors.Control;
+            this.lablshow.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lablshow.ForeColor = System.Drawing.Color.Gray;
+            this.lablshow.Location = new System.Drawing.Point(12, 8);
+            this.lablshow.Name = "lablshow";
+            this.lablshow.Size = new System.Drawing.Size(0, 17);
+            this.lablshow.TabIndex = 100;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 23);
+            this.textBox1.TabIndex = 99;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn2.Location = new System.Drawing.Point(56, 146);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(35, 35);
+            this.btn2.TabIndex = 97;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btntotal
+            // 
+            this.btntotal.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btntotal.Location = new System.Drawing.Point(185, 187);
+            this.btntotal.Name = "btntotal";
+            this.btntotal.Size = new System.Drawing.Size(50, 35);
+            this.btntotal.TabIndex = 96;
+            this.btntotal.Text = "=";
+            this.btntotal.UseVisualStyleBackColor = true;
+            this.btntotal.Click += new System.EventHandler(this.btntotal_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button15.Location = new System.Drawing.Point(142, 64);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(35, 35);
+            this.button15.TabIndex = 95;
+            this.button15.Tag = "/";
+            this.button15.Text = "/";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button14.Location = new System.Drawing.Point(142, 105);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(35, 35);
+            this.button14.TabIndex = 94;
+            this.button14.Tag = "*";
+            this.button14.Text = "*";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button13.Location = new System.Drawing.Point(142, 146);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(35, 35);
+            this.button13.TabIndex = 93;
+            this.button13.Tag = "-";
+            this.button13.Text = "-";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button12.Location = new System.Drawing.Point(142, 185);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(35, 35);
+            this.button12.TabIndex = 92;
+            this.button12.Tag = "+";
+            this.button12.Text = "+";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // dot
+            // 
+            this.dot.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dot.Location = new System.Drawing.Point(100, 185);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(35, 35);
+            this.dot.TabIndex = 91;
+            this.dot.Text = ".";
+            this.dot.UseVisualStyleBackColor = true;
+            this.dot.Click += new System.EventHandler(this.dot_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn9.Location = new System.Drawing.Point(100, 64);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(35, 35);
+            this.btn9.TabIndex = 90;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn8.Location = new System.Drawing.Point(56, 64);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(35, 35);
+            this.btn8.TabIndex = 89;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn7.Location = new System.Drawing.Point(12, 64);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(35, 35);
+            this.btn7.TabIndex = 88;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn6.Location = new System.Drawing.Point(100, 105);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(35, 35);
+            this.btn6.TabIndex = 87;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn5.Location = new System.Drawing.Point(56, 105);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(35, 35);
+            this.btn5.TabIndex = 86;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn4.Location = new System.Drawing.Point(12, 105);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(35, 35);
+            this.btn4.TabIndex = 85;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn3.Location = new System.Drawing.Point(100, 146);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(35, 35);
+            this.btn3.TabIndex = 84;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn1.Location = new System.Drawing.Point(12, 146);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(35, 35);
+            this.btn1.TabIndex = 83;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn0.Location = new System.Drawing.Point(12, 187);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(79, 33);
+            this.btn0.TabIndex = 82;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnnegative
+            // 
+            this.btnnegative.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnnegative.Location = new System.Drawing.Point(185, 64);
+            this.btnnegative.Name = "btnnegative";
+            this.btnnegative.Size = new System.Drawing.Size(50, 35);
+            this.btnnegative.TabIndex = 102;
+            this.btnnegative.Text = "+/-";
+            this.btnnegative.UseVisualStyleBackColor = true;
+            this.btnnegative.Click += new System.EventHandler(this.btnnegative_Click);
+            // 
+            // btnback
+            // 
+            this.btnback.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnback.Location = new System.Drawing.Point(185, 105);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(50, 35);
+            this.btnback.TabIndex = 103;
+            this.btnback.Text = " ←";
+            this.btnback.UseVisualStyleBackColor = true;
+            this.btnback.Click += new System.EventHandler(this.btnback_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 236);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.btnnegative);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.lablshow);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btntotal);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "小算盤";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Label lablshow;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btntotal;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button dot;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnnegative;
+        private System.Windows.Forms.Button btnback;
+    }
+}
+
---
+++ 小算盤-陣列/小算盤-陣列/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Windows.Forms;
+
+namespace 小算盤_陣列
+{
+    public partial class Form1 : Form
+    {
+      // int log = 0; //設定一個參數為0
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //用兩個list寫,一個存數字一個存符號
+        /// <summary>
+        /// 符號List
+        /// </summary>
+        private List<string> namestring = new List<string>(); //Array轉list //放在全域變數裡面 //放符號
+
+        /// <summary>
+        /// 數字List
+        /// </summary>
+        private List<double> namestring2 = new List<double>();//數字
+
+        private void btntotal_Click(object sender, EventArgs e)
+        {
+            ////分成兩個陣列 數字和運算值
+            ////char [] a= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };//int不用 ""or''
+            //char[] b = { '+', '-', '*', '//' };
+            ////b[0] = '+'; b[1] = '-'; b[2] = '*'; b[3] = '/'; //取出符號存放在陣列 ,陣列從0開始算
+            //string[] name = lablshow.Text.Split(b); //對暫存數字串的那格 做切割(Split) 把符號提出來分別放在陣列裡
+            ////string[] name2 = textBox1.Text.Split(a); //對textbox字串做切割(Split) 把數字提出來分別放在陣列裡
+            //List<string> namestring2 = new List<string>(name); //Array轉list //放數字
+            //if (log == 0)
+            //{
+
+            CheckNumber();
+
+
+            operation(namestring2, namestring); //進入運算 參數位置要一樣
+            namestring.Clear();//清除符號
+
+            if (namestring2.Count != 0)
+            {
+                lablshow.Text = namestring2[0].ToString(); //把運算完的數字(namestring2)放進labl顯示 //[0]正負號整數
+                textBox1.Text = namestring2[0].ToString();  //暫存上一個加總,ex.總值=7,下一個輸入2 = 7+2
+            }
+            namestring2.Clear();//清除數字
+            lablshow.Text = ""; //對按下等於後殘留在上方的數字做清除
+                               
+        }
+
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "";
+                lablshow.Text = "";
+            }
+            else if(textBox1.Text=="+"|| textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" )
+            {
+                lablshow.Text += textBox1.Text;
+                Add_substring(textBox1.Text);//呼叫方法 ("+")<=s的計算符號  
+                textBox1.Text = "";
+            }
+            textBox1.Text += ((Button)sender).Text;//按鍵顯示在textbox上
+        }
+
+
+        #region 四則運算
+        //做運算//建立新的群組 為運算用(operation意思:運算)  //()裡面放參數
+        double c; //總和
+        /// <summary>
+        /// 四則運算
+        /// <para>說明:第一個參數為數字LIST , 第二個參數為符號LIST</para>
+        /// </summary>
+        /// <param name="namestring2"></param>
+        /// <param name="namestring"></param>
+        public void operation(List<double> namestring2, List<string> namestring)
+        {
+            List<string> op = new List<string>(new string[] { "*", "/", "-", "+" });//建立新的list 放運算符號
+            foreach (string pp in op) //先乘除後加減
+            {
+                while (namestring.Contains(pp)) //找list裡面有沒有xx值,有的話進入迴圈
+                {
+                    int o = namestring.IndexOf(pp);
+                    int oo = namestring.IndexOf(pp) + 1; //namestng2 數字
+
+                    c = 0;
+                    double k = namestring2[o];//namestring符號
+                    double j = namestring2[oo];
+                    if (pp == "*")
+                    {
+                        c = k*j;
+                    }
+                    else if (pp == "/")
+                    {
+                        c = k/j;
+                    }
+                    else if (pp == "-")
+                    {
+                        c = k-j;
+                    }
+                    else
+
+                    {
+                        c = k+j;
+                    }
+                    //清除計算過數值
+                    namestring2.RemoveAt(oo);
+                    namestring.RemoveAt(o);
+                    namestring2[o] = c;
+                    if (namestring.Contains(pp))
+                    {
+                        continue;//如有相同運算符號,則回while
+                    }
+                }
+            }
+        }
+
+        #endregion
+        //_____________________________運算符號事件________________________________
+
+        #region 運算符號事件
+        /// <summary>
+        /// 新增運算符號
+        /// </summary>
+        /// <param name="s"></param>
+        public void Add_substring(string s) //放變數接收值 
+        {
+            namestring.Add(s);//增加陣列內容(s)到list裡面  //放符號
+        }
+
+        /// <summary>
+        /// 新增數字到list
+        /// </summary>
+        /// <param name="s"></param>
+        public void Add_number(double s) //放變數接收值
+        {
+            namestring2.Add(s);//增加陣列內容(s)到list裡面  //放數字
+        }
+
+        /// <summary>
+        /// 確認是否為數字，若是則加入數字list，並將值給予textbox1
+        /// </summary>
+        private void CheckNumber()
+        {
+            double n;
+            if (double.TryParse(textBox1.Text, out n))//判斷字串是否為數字
+            {
+                Add_number(Convert.ToDouble(textBox1.Text));
+                lablshow.Text += textBox1.Text;
+            }
+        }
+
+        #endregion
+
+       
+        private void button12_Click(object sender, EventArgs e)//運算按鍵
+        {
+            //HACK:新增確認數字的方法
+            CheckNumber();
+
+            //HACK:將運算的按鍵統一為BUTTON12
+            textBox1.Text = ((Button)sender).Text; 
+        }
+   
+        private void CE_Click(object sender, EventArgs e)//清除所有值
+        {
+            textBox1.Text = "";
+            lablshow.Text = "";
+            namestring.Clear();
+            namestring2.Clear();
+        }
+
+        private void dot_Click(object sender, EventArgs e) //小數點
+        {
+            if (textBox1.Text.IndexOf(".") < 0)
+            {
+                textBox1.Text += ".";
+            }
+        }
+
+        private void btnnegative_Click(object sender, EventArgs e)
+        {
+            double n;
+            if (double.TryParse(textBox1.Text, out n))//判斷字串是否為數字
+            {
+                if (textBox1.Text.IndexOf("-") >= 0)
+                {
+                    textBox1.Text = textBox1.Text.Replace("-", "");
+                }
+                else
+                {
+                    textBox1.Text = "-" + textBox1.Text;
+                }
+
+            }
+        }
+
+        private void btnback_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            if (s.Length > 1)
+            {
+                textBox1.Text = (double.Parse(s.Substring(0, s.Length - 1))).ToString();
+            }
+            else
+            {
+                textBox1.Text = "0"; 
+            }
+        }
+    }
+}
